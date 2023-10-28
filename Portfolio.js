3839a6858363494f0369bd0fde28e02412e4de58
Gaetano <romanog91@hotmail.it>
2023-10-28 17:18:58 +0100

    details tag fixed

--- Portfolio.js
+++
@@ -1,5 +0,0 @@
-// const showImageButton = document.getElementById("show-image-button");
-//       const myImage = document.getElementById("my-image");
-//      showImageButton.addEventListener("click", () => {
-//         myImage.hidden = !myImage.hidden;
-//      });
